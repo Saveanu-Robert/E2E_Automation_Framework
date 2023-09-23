80754740681575b31af18ac201eb6e1281311b8b
Robert Marcel SAVEANU <robert_saveanu@outlook.com>
2023-09-23 11:53:50 +0200

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,21 @@
-import { defineConfig, devices, PlaywrightTestConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
+
+const RPconfig = {
+  apiKey: 'playwright-api_IVVPOHxHRp2lFssXXHRDAfltVga6LPdwP10Asu8p764kCKHOCsT9H97pE_78cJDp',
+  endpoint: 'https:/demo.reportportal.io/api/v1',
+  project: 'default_personal',
+  launch: 'E2E_Automation_Framework',
+  attributes: [
+    {
+      key: 'key',
+      value: 'value',
+    },
+    {
+      value: 'value',
+    },
+  ],
+  description: 'This is a demo run',
+};
 
 export default defineConfig({
   testDir: './tests',
@@ -15,7 +32,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 12 : undefined,
 
-  reporter: 'allure-playwright',
+  reporter: [['allure-playwright'], ['@reportportal/agent-js-playwright', RPconfig]], 
 
   use: {
     baseURL: 'https://qacart-todo.herokuapp.com',
@@ -51,8 +68,5 @@ export default defineConfig({
        use: { ...devices['iPhone 12'] },
      },
   ],
-const config: PlaywrightTestConfig = {
-    reporter: [['@reportportal/agent-js-playwright', RPconfig]],
-    testDir: './tests',
-  };
+
 });
